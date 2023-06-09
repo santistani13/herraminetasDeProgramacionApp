73896aabc99485ea64c3b45b7404c58f37127f83
sstanicio <sstanicio@balanz.com>
2023-06-09 01:24:05 -0300

    ajustes en los textos

--- Models/Equipo.cs
+++ Models/Equipo.cs
@@ -8,8 +8,11 @@ public class Equipo {
     public int Id { get; set; }
     public string Nombre { get; set; }
     public string Pais { get; set; }
+     [Display(Name ="Fundación")]
+
     public int AnioFundacion {get; set;}
     public int Titulos { get; set; }
+    [Display(Name =" N° Socios")]
     public int CantidadSocios{ get; set; }
     
    public int LigaId { get; set; }
