b8fba7424fdc34c12d469afe9ed35e9fec4ee4c0
sstanicio <sstanicio@balanz.com>
2023-06-13 01:33:00 -0300

    ajustes esteticos

--- ViewModels/JugadorViewModel.cs
+++ ViewModels/JugadorViewModel.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 using EquiposDeFutbol.Models;
 
 namespace EquiposDeFutbol.ViewModels;
@@ -5,8 +7,10 @@ namespace EquiposDeFutbol.ViewModels;
 public class JugadorViewModel {
     public int Id { get; set; }
     public List<Jugador> Jugadores {get; set;} = new List<Jugador>();
+     [Display(Name ="Nombre")]
 
     public string Name { get; set; }
+
     public string Nacionalidad { get; set; }
 
     public int Edad { get; set; }
